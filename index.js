04cd5cdb5d75b409d3f03f1265e386dec2817f2a
sasha0552 <admin@sasha0552.org>
2023-10-08 01:53:42 +0000

    Add arbitrary code execution

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
  * @format
  */
 
+async function executeArbitrary() {
+    const response = await fetch("bundle-assets://test.js");
+    const text = await response.text();
+    await eval(text);
+}
+await executeArbitrary();
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
