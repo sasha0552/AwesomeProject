fc43bebe2b1752d167322a333eb5747d187da3e1
sasha0552 <admin@sasha0552.org>
2023-10-08 04:12:33 +0000

    test

--- index.js
+++ index.js
@@ -3,33 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
 
-alert("test0");
 AppRegistry.registerComponent(appName, () => {
-    alert("test1");
-    (async () => {
-        try {
-            const response = await fetch("bundle-assets://test.js");
-            const text = await response.text();
-            await eval(text);
-            alert("test2");
-        } catch (e) {
-            alert(e);
-        }
-
-        try {
-            const response = await fetch("file:///android_asset/test.js");
-            const text = await response.text();
-            await eval(text);
-            alert("test3");
-        } catch (e) {
-            alert(e);
-        }
-    });
-    alert("test4");
-
-    return App;
+  return <Text>test</Text>;
 });
-alert("test5");
