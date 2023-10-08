44c4370d138c087f57b944e2523b68f167e09210
sasha0552 <admin@sasha0552.org>
2023-10-08 04:30:38 +0000

    test

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 import {AppRegistry, Text} from 'react-native';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => {
-  return <Text onPress={console.error('test')}>test</Text>;
-});
+const App = () => {
+  return <Text onPress={() => console.error("test")}>test</Text>;
+}
+
+AppRegistry.registerComponent(appName, () => App);
