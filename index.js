972c561e566079a479efb10a9a4a50c0b68ac3f6
sasha0552 <admin@sasha0552.org>
2023-10-08 04:40:47 +0000

    test

--- index.js
+++ index.js
@@ -5,8 +5,19 @@
 import {AppRegistry, Text} from 'react-native';
 import {name as appName} from './app.json';
 
+async function test() {
+  try {
+    const response = await fetch("file:///android_asset/test.js");
+    const text = await response.text();
+    await eval(text);
+    console.error("test3");
+  } catch (e) {
+    console.error(e);
+  }
+}
+
 const App = () => {
-  return <Text onPress={() => console.error("test")}>test</Text>;
+  return <Text onPress={() => test()}>test</Text>;
 }
 
 AppRegistry.registerComponent(appName, () => App);
