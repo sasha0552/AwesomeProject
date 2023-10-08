3c78820ea84ce3b718aa1d53861484589fd92ba2
sasha0552 <admin@sasha0552.org>
2023-10-08 05:31:35 +0000

    test

--- android/app/src/main/java/com/awesomeproject/TestPackage.java
+++ android/app/src/main/java/com/awesomeproject/TestPackage.java
@@ -5,6 +5,10 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestPackage implements ReactPackage {
    @Override
    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
